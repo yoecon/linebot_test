b5fc011b1724216e2ee646f13e1446b7c2172dff
yoecon <yoecon@hotmail.com>
2022-05-04 16:21:43 +0700

    initial commit

---
+++ Project/Config.py
@@ -0,0 +1,3 @@
+Channel_secret = '3be237e8bb18d35379054674727c5790'
+Channel_access_token = 'va735begraeiuQCNljpEr8NnCBjYQ+UmfVoWfDRg16TJ/1zSYMLlIFTAKtSj2l6+5zrwlQhf94GnNUfH+CmycwHIRbMw5k3Px31e/SRgx+FvDbZ78bMaDyNdTqvPa3225wUcOxNSSkV4pGqJEZslWAdB04t89/1O/w1cDnyilFU='
+basic_id = '@319owufa'
---
+++ app.py
@@ -0,0 +1,4 @@
+from Project import app
+
+if __name__  == '__main__':
+    app.run(port = 3000) # forward port url to public url by ngrok
